ba8cec4dd77acb337a82663a269a2f44f1796194
geetu040 <raoarmaghanshakir040@gmail.com>
2026-09-16 15:33:15 +0200

    set `quantiles` as property

--- sktime/libs/chronos/chronos_bolt.py
+++ sktime/libs/chronos/chronos_bolt.py
@@ -675,6 +675,10 @@ class ChronosBoltPipeline:
     model: ChronosBoltModelForForecasting
     default_context_length: int = 2048
 
+    @property
+    def quantiles(self) -> list[float]:
+        return self.model.config.chronos_config["quantiles"]
+
     def _prepare_and_validate_context(self, context):
         if isinstance(context, list):
             context = left_pad_and_stack_1D(context)
